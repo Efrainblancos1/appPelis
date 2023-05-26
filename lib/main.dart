53486130460805974d93bf75c7cbb74bf19d80d2
Efrain Blanco <efrainblancos1@gmail.com>
2023-05-26 14:06:39 -0500

    enviroments

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(ProviderScope(child: const MyApp()));
+
 }
 
 class MyApp extends StatelessWidget {
@@ -18,6 +22,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: AppTheme().getTheme(),
       routerConfig: AppRouter,
+      
     );
   }
 }
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,18 +1,22 @@
 
+import 'package:cinemapedia/config/constans/enviroment.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
 // class HomeScreen extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   static const name='HomeScreen';
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context,) {
+  Widget build(BuildContext context,) {                                         
 
     return Scaffold(
       appBar: AppBar(title: Text('HomeScreen'),),
       body: Center(
-      
+        child: Text(Eviroment.theMovieDbKey),
       ),
      
     );
