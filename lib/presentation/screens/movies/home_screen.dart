47c2234f7b812670176bb76333c98c07225272cb
Efrain Blanco <efrainblancos1@gmail.com>
2023-06-01 14:39:24 -0500

    avance

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,10 +1,11 @@
 
 
+
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
-
+import 'package:cinemapedia/presentation/providers/providers.dart';
 class HomeScreen extends StatelessWidget {
 
   static const name = 'home-screen';
@@ -39,13 +40,15 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
 
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
-    if (nowPlayingMovies.length==0 ) return Center(child: CircularProgressIndicator());
+
+    final slidesShowMovies = ref.watch(moviesSlideshowProvider);
+
+    if (slidesShowMovies.length==0 ) return Center(child: CircularProgressIndicator());
 
     return Column(
       children: [
         CustomAppbar(),
-        MoviesSlideShow(movies: nowPlayingMovies),
+        MoviesSlideShow(movies: slidesShowMovies),
       ],
     );
   }
